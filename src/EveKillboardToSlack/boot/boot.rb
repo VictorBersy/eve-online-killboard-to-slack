664bb72b0adb68b171c75e9238762d279db83d1c
Victor Bersy <victor.bersy@gmail.com>
2015-07-28 23:38:21 +0200

    Set global vars at the boot

--- src/EveKillboardToSlack/boot/boot.rb
+++ src/EveKillboardToSlack/boot/boot.rb
@@ -2,6 +2,14 @@ module EveKillboardToSlack
   module Boot
     class Boot
       def initialize
+        set_global_vars
+        @zkillboard = Zkillboard::Zkillboard.new
+      end
+
+      def set_global_vars
+        $root_path = File.expand_path(File.join(__dir__, '..', '..', '..'))
+        $database  = YAML::load_file(File.join($root_path, 'config', 'database.yml'))
+        $config    = YAML::load_file(File.join($root_path, 'config', 'config.yml'))
       end
     end
   end
