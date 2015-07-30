f783e2024d9ffe9a6175e484dd9a8f9862e4f085
Victor Bersy <victor.bersy@gmail.com>
2015-07-31 01:16:24 +0200

    Add basic log formatter

---
+++ src/EveKillboardToSlack/messages/formatter/log.rb
@@ -0,0 +1,21 @@
+module EveKillboardToSlack
+  module Message
+    module Formatter
+      class LogFormatter
+        def initialize(data)
+          @data = data
+        end
+
+        def format
+          type            = @data[:type]
+          date            = @data[:time].strftime("%Y-%m-%d at %H:%M:%S (EvE Time)")
+          pilot_name      = @data[:pilot_name]
+          totalValue      = @data[:isk_value]
+          zkillboard_link = @data[:link]
+
+          "New #{type} : [#{date} : #{pilot_name} / #{totalValue}](#{zkillboard_link})"
+        end
+      end
+    end
+  end
+end
