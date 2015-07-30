841d4f33df0b8cdb1242d75018a6e309d95a6aa6
Victor Bersy <victor.bersy@gmail.com>
2015-07-31 01:18:53 +0200

    Move Message::Formatter to Message:Formatter:Formatter

--- src/EveKillboardToSlack/messages/formatter.rb
+++ src/EveKillboardToSlack/messages/formatter.rb
@@ -1,21 +1,29 @@
 module EveKillboardToSlack
   module Message
-    class Formatter
-      def initialize(data, params)
-        agnostic_data = data_to_agnostic_message(data)
-        puts params, agnostic_data
-      end
+    module Formatter
+      class Formatter
+        def initialize(data, formatter_name)
+          @agnostic_data = data_to_agnostic_format(data)
+          @formatter_name = formatter_name
+        end
 
-      private
+        def format_message
+          class_name = "EveKillboardToSlack::Message::Formatter::#{@formatter_name.downcase.capitalize}Formatter"
+          Object.const_get(class_name).new(@agnostic_data).format
+        end
 
-      def data_to_agnostic_message(data)
-        {
-          id: data['killID'],
-          time: Time.parse(data['killTime']).to_s,
-          pilot_name: data['victim']['characterName'],
-          isk_value: data['zkb']['totalValue'].to_s + ' ISK',
-          link: "https://zkillboard.com/kill/#{data['killID']}/"
-        }
+        private
+
+        def data_to_agnostic_format(data)
+          {
+            type: data[:type],
+            id: data['killID'],
+            time: Time.parse(data['killTime']),
+            pilot_name: data['victim']['characterName'],
+            isk_value: data['zkb']['totalValue'].to_s + ' ISK',
+            link: "https://zkillboard.com/kill/#{data['killID']}/"
+          }
+        end
       end
     end
   end
