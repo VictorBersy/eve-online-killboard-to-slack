b66b68d8ca38b029360ebdbd34429ab3167d4935
Victor Bersy <victor.bersy@gmail.com>
2015-07-31 23:14:18 +0200

    Save the last kill / loss id in database to filter the next API call

--- src/EveKillboardToSlack/zkillboard/zkillboard.rb
+++ src/EveKillboardToSlack/zkillboard/zkillboard.rb
@@ -2,6 +2,7 @@ module EveKillboardToSlack
   module Zkillboard
     class Zkillboard
       def initialize
+        @last_loss_id = {}
         retrieve_losses($config['zkillboard']['losses_url'])
         retrieve_kills($config['zkillboard']['kills_url'])
       end
@@ -15,7 +16,7 @@ module EveKillboardToSlack
       end
 
       def retrieve_data(type, url)
-        last_id = $database["last_#{type}_id".to_sym].to_s
+        last_id = $database.get("last_#{type}_id")
         url = append_zk_param(url, 'afterKillID', last_id)
         json_data = JSON.parse(open(url).read)
         split_data(json_data, type.to_sym)
@@ -30,6 +31,7 @@ module EveKillboardToSlack
 
       def split_data(json_data, type)
         json_data.each do |data|
+          save_last_id(data['killID'], type)
           format_message(data, type)
         end
       end
@@ -37,6 +39,11 @@ module EveKillboardToSlack
       def format_message(data, type)
         params = { to: [:slack], type: type }
         EveKillboardToSlack::Message::Dispatcher.new(data, params)
+      end
+
+      def save_last_id(last_id, type)
+        $database.set("last_#{type}_id", last_id) unless @last_loss_id[type]
+        @last_loss_id[type] = last_id
       end
     end
   end
