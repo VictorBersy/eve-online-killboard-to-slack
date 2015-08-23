e3f8e4d5e012ad625479abc138b817be8cf158bb
Victor Bersy <victor.bersy@gmail.com>
2015-08-23 21:40:06 +0200

    Unfurl media & URL into slack

--- src/EveKillboardToSlack/messages/sender/slack.rb
+++ src/EveKillboardToSlack/messages/sender/slack.rb
@@ -8,7 +8,8 @@ module EveKillboardToSlack
         end
 
         def send(message)
-          @slack_client.ping message
+          params = { unfurl_media: true, unfurl_links: true }
+          @slack_client.ping message, params
         end
 
         def create_slack_client
